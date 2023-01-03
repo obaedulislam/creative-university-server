8762c2de1843f7c8547c18ae2ecf398701899caf
obayedul islam <obaed.cse@gmail.com>
2023-01-03 14:25:19 +0600

    Server basic setup done

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config(); //To connect .env file
 const app = express();
 const port = process.env.PORT || 4900;
 
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ypkp6ia.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.get("/", (req, res) => {
   res.send("Academic Portal server running");
